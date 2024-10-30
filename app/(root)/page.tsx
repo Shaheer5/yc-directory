5e288d055c5795caefcf066e255c5d769e011d9d
Shaheer Mansoor <shaheermansoor321@gmail.com>
2024-10-30 18:39:47 +0500

    create post card design

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,24 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
   const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: {
+        _id: 12312,
+        name: "Shaheer Mansoor",
+      },
+      _id: 12311,
+      description: "this is description",
+      image:
+        "https://images.unsplash.com/photo-1593376893114-1aed528d80cf?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cm9ib3RzfGVufDB8fDB8fHww",
+      category: "Robots",
+      title: "We are robots",
+    },
+  ];
   return (
     <>
       <section className="pink_container">
@@ -13,6 +30,16 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
         </p>
 
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">{query ? `Search results for "${query}"` : "All Startups"}</p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => <StartupCard key={post?._id} post={post} />)
+          ) : (
+            <p className="no-results">No posts found</p>
+          )}
+        </ul>
       </section>
     </>
   );
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    // later on remove this
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
