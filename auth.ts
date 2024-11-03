902768e663ddb94a6243234d034da442cca8aee4
Shaheer Mansoor <shaheermansoor321@gmail.com>
2024-11-03 14:22:13 +0500

    add react-md-editor pkg, create "Create page" and Startup form component, implemented zod validation

--- auth.ts
+++ auth.ts
@@ -40,7 +40,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     },
     async session({ session, token }) {
       Object.assign(session, { id: token.id });
-      console.log("Session after modification:", session);
+      // console.log("Session after modification:", session);
       return session;
     },
   },
